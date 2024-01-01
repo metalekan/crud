dc8e8ab7b1cb13f101c6e77cd89872e23e6412ed
metalekan <metadelekan@gmail.com>
2024-01-01 17:14:31 +0100

    new commit

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,4 +2,5 @@ export const userList = [
     { "id": 1, "name": "Persis Faircloth", "email": "pfaircloth0@163.com" },
     { "id": 2, "name": "Vickie Hatfull", "email": "vhatfull1@pbs.org" },
     { "id": 3, "name": "Conant Borges", "email": "cborges2@answers.com" },
+    { "id": 4, "name": "John Developer", "email": "cborges2@answers.com" },
 ]
